1d5e14a38199aeaf4ff87dbde7de50afaa4b5cb7
Jude <973829691@qq.com>
2021-08-03 16:05:57 +0800

    improve

--- clientinfo-frontend-client/src/index.ts
+++ clientinfo-frontend-client/src/index.ts
@@ -1,4 +1,14 @@
 import ClientInfoInstanceFactory from './module/ClientInfoInstanceFactory'
+import ClientInfoInstance from "./domain/ClientInfoInstance";
 export {ClientInfoInstanceFactory}
 
 
+declare global {
+    interface Window {
+        info: ClientInfoInstance
+        extension: ClientInfoInstance
+    }
+}
+
+window.info = ClientInfoInstanceFactory.getInstance()
+window.extension = ClientInfoInstanceFactory.getInstance()
