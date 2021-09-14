c9a6843aaf9ecf323c5ea6573ec970b861eddaa6
sudongyu <976499226@qq.com>
2021-09-14 20:20:30 +0800

    chore:(ClientInfo) change class to interface & improve infojs-server

--- infojs-server/router_handler/clientInfo.js
+++ infojs-server/router_handler/clientInfo.js
@@ -1,7 +1,7 @@
 //获取客户端的ip地址
 module.exports.getClientIpAddress=(req,res) => {
     // console.log(req.ipv6Only)
-    res.send({
-        ipAddress:req.headers.host
-    })
+    const ipAddress=req.headers.host.substring(0,req.headers.host.indexOf(":"));
+    // console.log(ipAddress)
+    res.send(ipAddress)
 }
