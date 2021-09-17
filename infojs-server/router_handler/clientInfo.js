2a404911d299aa035508cbd4fdcad5e3c2b3dce9
sudongyu <976499226@qq.com>
2021-09-17 15:13:06 +0800

    chore(clientInfo): improve

--- infojs-server/router_handler/clientInfo.js
+++ infojs-server/router_handler/clientInfo.js
@@ -1,12 +1,10 @@
 //获取客户端的ip地址
 module.exports.getClientIpAddress=(req,res) => {
-
-    let ip=req.clientIp;
-    ip=ip.toString().replace("::ffff:","")
-    console.log('IPADDRESS',ip);
-    // console.log(req.ip)
-    // let ipAddress=req.headers.host;
-    // if(req.headers.host.indexOf(":")!==-1)
-    //     ipAddress =req.headers.host.substring(0,req.headers.host.indexOf(":"));
-    res.send(ip)
+    console.log(req.headers)
+    let ipAddress= req.headers["True-Client-IP"]??'unKnow'
+        let ipCountry= req.headers["CF-IPCountry"]??'unKnow'
+    res.json({
+        "ipAddress":ipAddress,
+        "ipCountry":ipCountry
+    })
 }
